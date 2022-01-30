7a1aef4a314f798196123d91f5b530a3d0dd204b
alba4k <blaskoazzolaaaron@gmail.com>
2022-01-30 18:13:42 +0100

    starting languaging

--- ShutdownTimer.py
+++ ShutdownTimer.py
@@ -3,24 +3,49 @@ import tkinter as tk
 from PIL import ImageTk
 import unixlib as buttons
 
-settings_json = open("shutdown/settings.json", "r+")
+settings_json = open("shutdown/settings.json", "r")
 settings = json.loads(settings_json.read())
 settings_json.close()
 
-lang_json = open("shutdown/lang/"+settings["lang"])
-lang = json.loads(lang_json.read())
-lang_json.close()
+def lang_check(lang, settings_display = None,theme = None,theme_desc = None):
+    settings["lang"] = lang
+    if(theme):
+        if(lang=="English"):
+            version.config(text=settings[settings["lang"]]["display.version"]+" 2.0\t\t\t         alba4k")
+            settings_display.title(settings[settings["lang"]]["settings.title"])
+            theme.config(settings[text=settings["lang"]]["settings.theme"])
+            theme_desc.config(settings[text=settings["lang"]]["settings.theme_desc"])
+        if(lang=="Italian"):
+            version.config(text=settings[settings["lang"]]["display.version"]+" 2.0\t\t\t         alba4k")
+            settings_display.title(settings[settings["lang"]]["settings.title"])
+            theme.config(settings[text=settings["lang"]]["settings.theme"])
+            theme_desc.config(settings[text=settings["lang"]]["settings.theme_desc"])
+        if(lang=="French"):
+            version.config(text=settings[settings["lang"]]["display.version"]+" 2.0\t\t\t         alba4k")
+            settings_display.title(settings[settings["lang"]]["settings.title"])
+            theme.config(settings[text=settings["lang"]]["settings.theme"])
+            theme_desc.config(settings[text=settings["lang"]]["settings.theme_desc"])
+        if(lang=="Spanish"):
+            version.config(text=settings[settings["lang"]]["display.version"]+" 2.0\t\t\t         alba4k")
+            settings_display.title(settings[settings["lang"]]["settings.title"])
+            theme.config(settings[text=settings["lang"]]["settings.theme"])
+            theme_desc.config(settings[text=settings["lang"]]["settings.theme_desc"])
+    else:
+        if(settings["lang"]=="English"):
+            version.config(text=settings[settings["lang"]]["display.version"]+" 2.0\t\t\t         alba4k")
+        if(settings["lang"]=="Italian"):
+            version.config(text=settings[settings["lang"]]["display.version"]+" 2.0\t\t\t         alba4k")
+        if(settings["lang"]=="French"):
+            version.config(text=settings[settings["lang"]]["display.version"]+" 2.0\t\t\t         alba4k")
+        if(settings["lang"]=="Spanish"):
+            version.config(text=settings[settings["lang"]]["display.version"]+" 2.0\t\t\t         alba4k")
 
-def theme_check(theme = None,
-    settings_display = None,
-    theme_desc = None,
-    lang_select=None,
-    change_settings = False,):
+def theme_check(theme = None,settings_display = None,theme_desc = None,lang_select=None,change_settings = False):
     if(change_settings):   # checks if it should change the settings.json file
         settings["usingDarkTheme"] = not settings["usingDarkTheme"]
     if(settings["usingDarkTheme"]):           # dark theme is on (primary window, display)
         display.config(bg = "#1C1C1C")
-        version.config(bg="#1C1C1C", fg="#F0F0F0", text=lang["display.version"])
+        version.config(bg="#1C1C1C", fg="#F0F0F0")
         cancel.config(bg="#1C1C1C", activebackground = "#1C1C1C")
         hours_entry.config(bg = "#252525", fg = "#F0F0F0")
         minutes_entry.config(bg = "#252525", fg = "#F0F0F0")
@@ -32,18 +57,15 @@ def theme_check(theme = None,
         logout_button.config(bg="#1C1C1C", activebackground = "#1C1C1C")
         settings_show.config(bg="#1C1C1C", activebackground="#1C1C1C", image = white_settings_icon)
 
-        try:    # Apply dark theme to the secondary window, if shown
+        if(theme):    # Apply dark theme to the secondary window, if shown
             settings_display.config(bg="#1C1C1C")
-            settings_display.title(lang["settings.title"])
-            theme.config(bg="#272727", fg = "#F0F0F0", activebackground = "#202020", activeforeground="#F0F0F0",text=lang["settings.theme"])
-            theme_desc.config(bg = "#1c1c1c", fg = "#f0f0f0",text=lang["settings.theme_desc"])
+            theme.config(bg="#272727", fg = "#F0F0F0", activebackground = "#202020", activeforeground="#F0F0F0")
+            theme_desc.config(bg = "#1c1c1c", fg = "#f0f0f0")
             lang_select.config(bg = "#1c1c1c", fg = "#f0f0f0", activebackground = "#1c1c1c", activeforeground="#F0F0F0", highlightthickness=0)
             lang_select["menu"].config(bg = "#1c1c1c", fg = "#f0f0f0", activebackground = "#222299", activeforeground="#F0F0F0")
-        except:
-            pass
     else:    # light theme is on (primary window, display)
         display.config(bg = "#F0F0F0")
-        version.config(bg = "#F0F0F0", fg="#000000", text = lang["display.version"])
+        version.config(bg = "#F0F0F0", fg="#000000")
         cancel.config(bg = "#F0F0F0", activebackground="#F0F0F0")
         hours_entry.config(bg = "#DADADA", fg="#000000")
         minutes_entry.config(bg = "#DADADA", fg="#000000")
@@ -55,21 +77,18 @@ def theme_check(theme = None,
         logout_button.config(bg="#F0F0F0", activebackground = "#F0F0F0")
         settings_show.config(bg = "#F0F0F0", activebackground="#F0F0F0", image = black_settings_icon)
 
-        try:    # Apply light theme to the secondary window, if shown
+        if(theme):    # Apply light theme to the secondary window, if shown
             settings_display.config(bg="#F0F0F0")
-            settings_display.title(lang["settings.title"])
             theme.config(bg = "#F0F0F0", fg="#000000", activebackground="#F0F0F0", activeforeground="#000000", text=lang["settings.theme"])
-            theme_desc.config(bg = "#f0f0f0", fg = "#000000", text=lang["settings.theme_desc"])
+            theme_desc.config(bg = "#f0f0f0", fg = "#000000")
             lang_select.config(bg = "#f0f0f0", fg = "#000000", activebackground="#f0f0f0", activeforeground="#000000", highlightthickness=0)
             lang_select["menu"].config(bg = "#f0f0f0", fg = "#000000", activebackground="#9999FF", activeforeground="#000000")
-        except:
-            pass
 
 def open_settings():
     settings_display = tk.Tk()
-    settings_display.geometry("280x200")
+    settings_display.geometry("300x200")
     lang_choice = tk.StringVar(settings_display)
-    lang_choice.set(lang["lang_name"])
+    lang_choice.set(settings["lang"])
 
     lang_select = tk.OptionMenu(settings_display,
         lang_choice,
@@ -81,7 +100,7 @@ def open_settings():
         font = ("Helvetica, 12"),
         highlightthickness=0
     )
-    theme_desc.place(x=70, y=55)
+    theme_desc.place(x=90, y=55)
 
     theme = tk.Button(settings_display,
         command=lambda: theme_check(theme,
@@ -92,14 +111,24 @@ def open_settings():
         ),
         highlightthickness=0
     )
-    theme.place(x=15, y=60)
 
+    apply = tk.Button(settings_display,
+        command=lambda: lang_check(lang_choice.get(),
+            settings_display,
+            theme_desc,
+            lang_select,
+            True
+        ),
+        highlightthickness=0
+    )
+
+    theme.place(x=15, y=60)
     theme_check(theme, settings_display, theme_desc, lang_select)
+    lang_check(settings["lang"], settings_display, theme, theme_desc)
 
 display=tk.Tk(className="\nShutdown")
 display.geometry("400x250")
 #display.iconbitmap("shutdown/assets/icon.ico")
-#display.overrideredirect(1)
 hours,minutes,seconds=tk.IntVar(),tk.IntVar(),tk.IntVar()
 
 shutdown_icon = ImageTk.PhotoImage(file="shutdown/assets/icons/shutdown.png")
@@ -180,8 +209,9 @@ seconds_entry.place(x=200, y=50, width=50, height=50)
 
 open_settings() #when testing in the secondary window
 theme_check()
+lang_check(settings["lang"])
 display.mainloop()
 
 settings_json = open("shutdown/settings.json", "w")
-settings_json.write(json.dumps(settings, indent = 2))
+settings_json.write(json.dumps(settings, indent = 4))
 settings_json.close()
